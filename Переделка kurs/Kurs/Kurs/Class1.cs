948cd71e491f7b5a2d6dec08f36d0f6657350809
kirra <124894387+kiryalemeshev@users.noreply.github.com>
2024-04-16 09:54:42 +0300

    Add files via upload

---
+++ Переделка kurs/Kurs/Kurs/Class1.cs
@@ -0,0 +1,298 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Management;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+using System.Windows.Forms;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Diagnostics; //!!!
+using System.Management; //!!!
+using Microsoft.VisualBasic; //!!!
+
+namespace Kurs
+{
+    internal class Class1
+    {
+
+
+        private List<Process> processes = null; //Главный список хранения процессов
+
+        private ListViewItemComparer comparer = null;
+
+
+
+        private void GetProcesses() //метод заполнения списка и его заполнения
+        {
+            processes.Clear(); //очистка списка
+
+            processes = Process.GetProcesses().ToList<Process>();     //заполнение списка заново и получаем все системные процессы GetProcesses()
+        }
+
+        private void RefreshProcessesList() //метод заполнения ListView контента
+        {
+
+            listView1.Items.Clear(); //Очистка
+
+            double memSize = 0; //В этой переменной будет при переборе всех процессов храниться память, которую занимает процессор
+
+            foreach (Process p in processes)
+            { //Перебор всех процессов
+
+                memSize = 0;
+
+                PerformanceCounter pc = new PerformanceCounter();
+                pc.CategoryName = "Process";
+                pc.CounterName = "Working Set - Private";
+                pc.InstanceName = p.ProcessName;
+
+                memSize = (double)pc.NextValue() / (1000 * 1000);
+
+                string[] row = new string[]
+                {p.ProcessName.ToString(), Math.Round(memSize,1).ToString()
+                };
+
+                listView1.Items.Add(new ListViewItem(row));
+
+                pc.Close();
+                pc.Dispose();
+            }
+
+            Text = "Запущено процессов: " + processes.Count.ToString();
+
+        }
+
+
+        private void RefreshProcessesList(List<Process> processes, string keyword)
+        {
+            try
+            {
+                listView1.Items.Clear();
+
+                double memSize = 0;
+
+                foreach (Process p in processes)
+                {
+
+                    if (p != null)
+                    {
+                        memSize = 0;
+
+                        PerformanceCounter pc = new PerformanceCounter();
+                        pc.CategoryName = "Process";
+                        pc.CounterName = "Working Set - Private";
+                        pc.InstanceName = p.ProcessName;
+
+                        memSize = (double)pc.NextValue() / (1000 * 1000);
+
+                        string[] row = new string[]
+                        {
+                            p.ProcessName.ToString(), Math.Round(memSize,1).ToString()
+                        };
+
+                        listView1.Items.Add(new ListViewItem(row));
+
+                        pc.Close();
+                        pc.Dispose();
+                    }
+
+                }
+
+                Text = $"Запущено процессов '{keyword}': " + processes.Count.ToString();
+            }
+            catch (Exception) { }
+
+        }
+
+
+
+
+        private void KillProcesses(Process processes)
+        {
+            processes.Kill();
+
+            processes.WaitForExit();
+        }
+
+
+        private void KillProcessesAndChildren(int pid)
+        {
+            if (pid == 0)
+                return;
+
+
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher(
+                "Select + From Win32_Process Where ParentProcessID=" + pid);
+
+            ManagementObjectCollection objectsCollection = searcher.Get();
+
+            foreach (ManagementObject obj in objectsCollection)
+            {
+                KillProcessesAndChildren(Convert.ToInt32(obj["ProcessId"]));
+            }
+
+            try
+            {
+
+                Process p = Process.GetProcessById(pid);
+
+                p.Kill();
+                p.WaitForExit();
+
+            }
+            catch (ArgumentException) { }
+        }
+
+
+        private int GetParentProccesId(Process p)
+        {
+            int parentId = 0;
+
+            try
+            {
+                ManagementObject managementObject = new ManagementObject("win32_procces.handle='" + p.Id + "'");
+
+                managementObject.Get();
+
+                parentId = Convert.ToInt32(managementObject["ParentProcessId"]);
+            }
+            catch (Exception) { }
+            return parentId;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            processes = new List<Process>();
+
+            GetProcesses();
+            RefreshProcessesList();
+
+
+            comparer = new ListViewItemComparer();
+            comparer.ColumnIndex = 0;
+        }
+
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {
+            GetProcesses(); RefreshProcessesList();
+        }
+
+        private void toolStripButton2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (listView1.SelectedItems[0] != null)
+                {
+                    Process processToKill = processes.Where((x) => x.ProcessName ==
+                    listView1.SelectedItems[0].SubItems[0].Text).ToList()[0];
+
+                    KillProcesses(processToKill);
+
+                    GetProcesses(); RefreshProcessesList();
+                }
+            }
+            catch (Exception) { }
+
+        }
+
+        private void toolStripButton3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (listView1.SelectedItems[0] != null)
+                {
+                    Process processToKill = processes.Where((x) => x.ProcessName ==
+                    listView1.SelectedItems[0].SubItems[0].Text).ToList()[0];
+
+                    KillProcessesAndChildren(GetParentProccesId(processToKill));
+
+                    GetProcesses(); RefreshProcessesList();
+
+
+                }
+            }
+            catch (Exception) { }
+        }
+
+        private void завершитьДеревоПроцессовToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (listView1.SelectedItems[0] != null)
+                {
+                    Process processToKill = processes.Where((x) => x.ProcessName ==
+                    listView1.SelectedItems[0].SubItems[0].Text).ToList()[0];
+
+                    KillProcessesAndChildren(GetParentProccesId(processToKill));
+
+                    GetProcesses(); RefreshProcessesList();
+
+
+                }
+            }
+            catch (Exception) { }
+        }
+
+        private void запуститьЗадачуToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string path = Interaction.InputBox("Введите имя программы", "Запуск новой задачи");
+
+            try
+            {
+                Process.Start(path);
+            }
+            catch (Exception) { }
+        }
+
+        private void toolStripTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            GetProcesses();
+
+            List<Process> filteredprocesses = processes.Where((x) =>
+            x.ProcessName.ToLower().Contains(toolStripTextBox1.Text.ToLower())).ToList<Process>();
+
+            RefreshProcessesList(filteredprocesses, toolStripTextBox1.Text);
+
+        }
+
+        private void завершитьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (listView1.SelectedItems[0] != null)
+                {
+                    Process processToKill = processes.Where((x) => x.ProcessName ==
+                    listView1.SelectedItems[0].SubItems[0].Text).ToList()[0];
+
+                    KillProcesses(processToKill);
+
+                    GetProcesses(); RefreshProcessesList();
+                }
+            }
+            catch (Exception) { }
+        }
+
+        private void listView1_ColumnClick(object sender, ColumnClickEventArgs e)
+        {
+            comparer.ColumnIndex = e.Column;
+            comparer.SortDirection = comparer.SortDirection == SortOrder.Ascending ? SortOrder.Descending : SortOrder.Ascending;
+
+            listView1.ListViewItemSorter = comparer;
+            Form1.listView1.Sort();
+        }
+
+        private void выходToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+        }
+    }
+
+
+
+
+}
---
+++ Переделка kurs/Kurs/Kurs/Form2.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MetroFramework.Forms;
+using System.Runtime.InteropServices;
+
+namespace Kurs
+{
+    public partial class Form2 : MetroForm
+    {
+
+        private float cpu;
+
+        private float ram;
+
+        private ulong installedMemory;
+
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            MEMORYSTATUSEX mEMORYSTATUSEX = new MEMORYSTATUSEX();
+
+            if(GlobalMemoryStatusEx(mEMORYSTATUSEX))
+            {
+                installedMemory = mEMORYSTATUSEX.ullTotalPhys;
+            }
+
+            metroLabel10.Text = Convert.ToString(installedMemory  / 1000000000) + "Гб";
+
+            timer1.Interval = 1000;
+            timer1.Start();
+        }
+
+
+
+        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Auto)]
+
+        private class MEMORYSTATUSEX
+        {
+            public uint dwLength;
+            public uint dwMemoryLength;
+            public ulong ullTotalPhys;
+            public ulong ullAvailPhys;
+            public ulong ullTotalPageFile;
+            public ulong ullAvailPageFile;
+            public ulong ullTotalVirtual;
+            public ulong ullAvailVirtual;
+            public ulong ullAvailExtendedVirtual;
+
+
+
+            public MEMORYSTATUSEX()
+            {
+                this.dwLength = (uint)Marshal.SizeOf(typeof(MEMORYSTATUSEX));
+            }
+
+        }
+
+
+        [return: MarshalAs(UnmanagedType.Bool)]
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+
+        static extern bool GlobalMemoryStatusEx([In, Out] MEMORYSTATUSEX lpBuffer);
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            cpu = performanceCPU.NextValue(); //Считывает данные с perfomancecouner
+            ram = performanceRAM.NextValue(); //Считывает данные с perfomancecouner
+
+            metroProgressBar1.Value = (int)cpu; //Присвоим значения в metroProgressBar1
+            metroProgressBar2.Value = (int)ram; //Присвоим значения в metroProgressBar2
+
+            metroLabel2.Text = Convert.ToString(Math.Round(cpu, 1)) + "%"; //Конвертируем в текст и переводим с точностью до 1 знака после запятой
+            metroLabel3.Text = Convert.ToString(Math.Round(ram, 1)) + "%";
+
+            metroLabel6.Text = Convert.ToString(Math.Round((ram / 100 * installedMemory) / 1000000000, 1)) + " Гб";
+           // metroLabel8.Text = Convert.ToString(Math.Round((installedMemory - ram / 100 * installedMemory) / 1000000000, 1)) +" Гб";
+
+
+            chart1.Series["ЦП"].Points.AddY(cpu); //Обращаемся к линиям графика как к ключу словаря через ЦП
+            chart1.Series["ОЗУ"].Points.AddY(ram); //Обращаемся к линиям графика как к ключу словаря через ОЗУ
+
+        }
+
+        private void metroLabel8_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+}
